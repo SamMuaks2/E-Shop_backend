a21af5e24ce3dd69acacae34276228074674cfe6
Mohri Samuel Muakpo <sammuaks@gmail.com>
2024-12-28 20:18:19 +0100

    Add file upload function

--- backend/index.js
+++ backend/index.js
@@ -13,9 +13,39 @@ const port = 4000;
 app.use(express.json());
 app.use(cors());
 
+// MongoDb connection
+mongoose.connect("mongodb+srv://sammuaks:broski2024@e-shop-app-0.dp5zh.mongodb.net/e-shopApp")
 
+// API endpoints
+app.get("/", (req, res) => {
+    res.send("Express App is running")
+})
 
+// Multer engine for image storage
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+const upload = multer({storage: storage})
+
+// Image upload endpoint
+app.use("/images", express.static('upload/images'))
+
+app.post("/upload", upload.single('product'), (req, res) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+app.listen(port, (error) => {
+    if (!error) {
+        console.log(`Server is running on port ${port}`);
+    }
+    else {
+        console.log("Error : " +error);
+    }
   });
