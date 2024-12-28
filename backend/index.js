f616d40adf1d334d84ec4189c7eda1e88b25e5f1
Mohri Samuel Muakpo <sammuaks@gmail.com>
2024-12-28 17:02:41 +0100

    Add dependencies

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import multer from "multer";
+import path from "path";
+import cors from "cors";
+
+
+const app = express();
+
+const port = 4000;
+
+app.use(express.json());
+app.use(cors());
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
