cadec88781515e48bef4ae976aebfec66d653592
Mohri Samuel Muakpo <sammuaks@gmail.com>
2024-12-31 20:51:33 +0100

    Add product page

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
-import "./AddProduct"
+import "./AddProduct.css"
+import UploadArea from "../../assets/upload_area.svg"
 
 const AddProduct = () => {
   return (
     <div className="add-product">
-        Add product
+        <div className="addproduct-itemfield">
+          <p>Product title</p>
+          <input type="text" name='name' placeholder='Type here' />
+        </div>
+
+        <div className="addproduct-price">
+          <div className="addproduct-itemfield">
+            <p>Price</p>
+            <input type="text" name='old_price' placeholder='Type here' />
+          </div>
+          
+          <div className="addproduct-itemfield">
+            <p>Offer Price</p>
+            <input type="text" name='new_price' placeholder='Type here' />
+          </div>
+        </div>
+
+        <div className="addproduct-itemfield">
+          <p>Product Category</p>
+          <select name="category" className='add-product-selector'>
+            <option value="women">Women</option>
+            <option value="men">Men</option>
+            <option value="kid">Kid</option>
+          </select>
+        </div>
+
+        <div className="addproduct-itemfield">
+          <label htmlFor="file-input">
+            <img src={UploadArea} className='addproduct-thumbnail-img' alt="" />
+          </label>
+          <input type="file" name='image' id='file-input' hidden />
+        </div>
+
+        <button className='addproduct-btn'>ADD</button>
     </div>
   )
 }
