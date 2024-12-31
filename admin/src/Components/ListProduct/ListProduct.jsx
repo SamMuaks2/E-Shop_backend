3f853950bf696dad5851cf4cef6115e467f707f2
Mohri Samuel Muakpo <sammuaks@gmail.com>
2024-12-31 16:05:30 +0100

    Add routes to admin page

---
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./ListProduct"
+
+const ListProduct = () => {
+  return (
+    <div className="list-product">
+        List product
+    </div>
+  )
+}
+
+export default ListProduct
