eab98094da5c75b6cde662a0814aab15989afb2c
Mohri Samuel Muakpo <sammuaks@gmail.com>
2025-01-02 12:52:51 +0100

    Start list product component

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./ListProduct"
 
 const ListProduct = () => {
+  const [allproducts, setAllproducts] = useState([]);
+
+  const fetchInfo = async () => {
+    await fetch('http://localhost:4000/allproducts').then((res) => res.json()).then((data) => {setAllproducts(data)});
+  }
+
+  useEffect(() => {
+    fetchInfo();
+  }, [])
+
   return (
     <div className="list-product">
-        List product
+        <h1>Products List</h1>
+
+        <div className="listproduct-format-main">
+          <p>Products</p>
+          <p>Title</p>
+          <p>Old Price</p>
+          <p>New Price</p>
+          <p>Category</p>
+          <p>Remove</p>
+        </div>
+
+        <div className="listproduct-allproducts">
+          <hr />
+
+          {allproducts.map}
+        </div>
     </div>
   )
 }
