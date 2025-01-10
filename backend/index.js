69fcf039fcc6f6e484c2bb3462d74792762862c4
Mohri Samuel Muakpo <sammuaks@gmail.com>
2025-01-10 15:35:26 +0100

    Add login endpoint

--- backend/index.js
+++ backend/index.js
@@ -195,6 +195,36 @@ app.post('/signup', async (req, res) => {
     res.json({success: true, token})
 })
 
+
+// User login endpoint
+app.post("/login", async (req, res) => {
+    let user = await Users.findOne({email: req.body.email});
+
+    if (user) {
+        const passCompare = req.body.password === user.password;
+
+        if (passCompare) {
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            const token =jwt.sign(data, 'secret_ecom');
+
+            res.json({success: true, token});
+        }
+
+        else {
+            res.json({success: false, errors: "Password incorrect"});
+        }
+    }
+
+    else {
+        res.json({success: false, errors: "Wrong email address"})
+    }
+})
+
 app.listen(port, (error) => {
     if (!error) {
         console.log(`Server is running on port ${port}`);
